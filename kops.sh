1c26f599d3da9168cc61af80118ca41d770a4e71
saikiran05-hub <arrollasaikiran25@gmail.com>
2025-02-21 23:27:44 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -11,8 +11,8 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket kiran0523.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket kiran0523.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://kiran0523.k8s.local
-kops create cluster --name sai.k8s.local --zones us-east-1a,us-east-1b --master-count 1 --master-size t2.medium --master-volume-size 25 --node-count 2 --node-size t2.micro --node-volume-size 20
-kops update cluster --name sai.k8s.local --yes --admin
+aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
+kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+kops update cluster --name rahams.k8s.local --yes --admin
